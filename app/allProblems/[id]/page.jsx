e5ecffb5e6a4e08404cba442462d25e65b39e4e7
darshil89 <darshilmahraur67@gmail.com>
2023-09-30 22:47:19 +0530

    route protected

--- app/allProblems/[id]/page.jsx
+++ app/allProblems/[id]/page.jsx
@@ -1,10 +1,13 @@
 "use client";
-
+import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { SyncLoader } from "react-spinners";
+import { useRouter } from "next/navigation";
 export default function Problem({ params: { id } }) {
   const [problem, setProblem] = useState(null);
   const [user, setUser] = useState(null);
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -45,29 +48,38 @@ export default function Problem({ params: { id } }) {
 
     fetchData();
   }, [id]);
+  if (session) {
+    if (!problem || !user) {
+      return <SyncLoader className="text-center mt-10" color="#2e3634" />;
+    }
 
-  if (!problem || !user) {
-    return <SyncLoader className="text-center mt-10" color="#2e3634" />;
+    const {
+      title,
+      content,
+      floorNumber,
+      roomNumber,
+      buildingNumber,
+      createdAt,
+    } = problem.problem;
+
+    const { name, email } = user.user;
+
+    return (
+      <>
+        <div className="text-xl">Problem</div>
+        <div>My Problem id is = {id}</div>
+        <div>Time = {createdAt}</div>
+        <div>My Problem title is = {title}</div>
+        <div>My Problem content is = {content}</div>
+        <div>My Problem floorNumber is = {floorNumber}</div>
+        <div>My Problem roomNumber is = {roomNumber}</div>
+        <div>My Problem buildingNumber is = {buildingNumber}</div>
+        <div className="text-xl">User</div>
+        <div>User Name = {name}</div>
+        <div>User Email = {email}</div>
+      </>
+    );
+  } else {
+    router.push("/signin");
   }
-
-  const { title, content, floorNumber, roomNumber, buildingNumber, createdAt } =
-    problem.problem;
-
-  const { name, email } = user.user;
-
-  return (
-    <>
-      <div className="text-xl">Problem</div>
-      <div>My Problem id is = {id}</div>
-      <div>Time = {createdAt}</div>
-      <div>My Problem title is = {title}</div>
-      <div>My Problem content is = {content}</div>
-      <div>My Problem floorNumber is = {floorNumber}</div>
-      <div>My Problem roomNumber is = {roomNumber}</div>
-      <div>My Problem buildingNumber is = {buildingNumber}</div>
-      <div className="text-xl">User</div>
-      <div>User Name = {name}</div>
-      <div>User Email = {email}</div>
-    </>
-  );
 }
