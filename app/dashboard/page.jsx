cbba59ecad86e79d1aea3c0d9574b24049c56117
darshil89 <darshilmahraur67@gmail.com>
2023-10-15 15:19:40 +0530

    admin dashboard error fixed

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,7 +2,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import UserDashboard from "@/components/dashboard/userDashboard";
-import CopyButton from "@/components/copyButton/page";
+ 
 const Dashboard = async () => {
   const session = await getServerSession(authOptions);
   if (typeof window !== "undefined") return null;
@@ -15,7 +15,7 @@ const Dashboard = async () => {
     return (
       <>
         <h1 className="text-center text-5xl">Hi user {session?.user.name}</h1>
-       
+       <UserDashboard />
 
       </>
     );
