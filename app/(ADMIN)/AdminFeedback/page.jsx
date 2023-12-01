faa2d0603e2a1bfe5647410ed4211473e7d485de
Darshil Mahraur <darshilmahraur67@gmail.com>
2023-12-01 12:03:07 +0530

    made production ready

--- app/(ADMIN)/AdminFeedback/page.jsx
+++ app/(ADMIN)/AdminFeedback/page.jsx
@@ -2,44 +2,12 @@ import CopyButton from "@/components/copyButton/page";
 import { authOptions } from "../../api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-
-let apiUrl;
-
-if (typeof window !== "undefined") {
-  // Determine the current port dynamically in the browser context
-  const currentPort =
-    window.location.port ||
-    (window.location.protocol === "https:" ? "443" : "80");
-
-  // Construct the API URL with the determined port
-  apiUrl = `${window.location.protocol}//${window.location.hostname}:${currentPort}/api/feedback`;
-} else {
-  // Handle the case when running on the server (no window object)
-  // You might want to provide a default API URL or throw an error based on your needs.
-  apiUrl = "http://localhost:3000/api/feedback";
-}
-
-const getAllFeedback = async () => {
-  try {
-    const response = await fetch(apiUrl, {
-      method: "GET",
-      next: {
-        revalidate: 0,
-      },
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log("error from file = ",error);
-  }
-};
+import { connectToDb } from "@/utils";
+import prisma from "@/prisma";
 
 export default async function AdminFeedback() {
-  const feedbacks = await getAllFeedback();
+  await connectToDb();
+  const feedbacks = await prisma.feedback.findMany();
 
   const session = await getServerSession(authOptions);
   if (typeof window !== "undefined") return null;
