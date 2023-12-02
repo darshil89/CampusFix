981fa33aceb7534435e38974e589c6154cca5915
Darshil Mahraur <darshilmahraur67@gmail.com>
2023-12-02 13:03:12 +0530

    UPDATED NAVBAR AGAIN

--- components/Navbar/main-header.jsx
+++ components/Navbar/main-header.jsx
@@ -21,55 +21,55 @@ function MainHeader() {
   };
 
   return (
-    <header className={classes.header}>
-      <div className={classes.logo}>
-        <Image
-          src="/images/logo.svg"
-          className="ml-32 h-fit"
-          alt="logo"
-          width={100}
-          height={100}
-        />
-        <Link href="/" className="ml-6">
-          DSCE
-        </Link>
-      </div>
+    <div className={classes.box}>
+      <header className={classes.header}>
+        <div className={classes.logo}>
+          <Image
+            src="/images/logo.svg"
+            className="ml-32 h-fit"
+            alt="logo"
+            width={100}
+            height={100}
+          />
+          <Link href="/" className="ml-6">
+            DSCE
+          </Link>
+        </div>
 
-      <div className={classes.lists}>
-        <Link href="/">Home</Link>
-        {checkAdmin && (
-          <>
-            <Link href="/allProblems">Dashboard</Link>
-            <Link href="/allUsers">Users</Link>
-            <Link href="/AdminFeedback">Feedback</Link>
-            <button className={classes.btn} onClick={handleSignOut}>
-              Sign Out
-            </button>
-          </>
-        )}
+        <div className={classes.lists}>
+          <Link href="/">Home</Link>
+          {checkAdmin && (
+            <>
+              <Link href="/allProblems">Dashboard</Link>
+              <Link href="/allUsers">Users</Link>
+              <Link href="/AdminFeedback">Feedback</Link>
+              <button className={classes.btn} onClick={handleSignOut}>
+                Sign Out
+              </button>
+            </>
+          )}
 
-        
+          {!session && (
+            <>
+              <Link href="/signup">Sign Up</Link>
 
-        {!session && (
-          <>
-            <Link href="/signup">Sign Up</Link>
-
-            <Link href="/signin">Sign In</Link>
-          </>
-        )}
-        {session && !checkAdmin && (
-          <>
-            <Link href="/dashboard">Dashboard</Link>
-            <Link href="/problemStatement"> Problem </Link>
-            <Link href="/feedback">Feedback</Link>
-            <Link href="/notification">Notification</Link>
-            <button className={classes.btn} onClick={handleSignOut}>
-              Sign Out
-            </button>
-          </>
-        )}
-      </div>
-    </header>
+              <Link href="/signin">Sign In</Link>
+            </>
+          )}
+          {session && !checkAdmin && (
+            <>
+              <Link href="/dashboard">Dashboard</Link>
+              <Link href="/problemStatement"> Problem </Link>
+              <Link href="/feedback">Feedback</Link>
+              <Link href="/notification">Notification</Link>
+              <button className={classes.btn} onClick={handleSignOut}>
+                Sign Out
+              </button>
+            </>
+          )}
+        </div>
+      </header>
+    </div>
   );
 }
 
