11f162aab6661ed20fc370417699b939799e7fbe
Niruth Ananth <69809343+Niruth4405@users.noreply.github.com>
2023-12-11 16:44:53 +0530

    ..

--- app/(ADMIN)/AdminFeedback/page.jsx
+++ app/(ADMIN)/AdminFeedback/page.jsx
@@ -17,27 +17,28 @@ export default async function AdminFeedback() {
   if (!session && email !== process.env.ADMIN_EMAIL) redirect("/signin");
   return (
     <>
-      <div className="flex justify-between mt-8 ml-4 mb-4 flex-row">
+      <div className="flex justify-between mt-8 ml-4 mb-4 ">
         <div className="text-4xl  text-gray-700 font-semibold">
           All User&apos;s <span className="text-blue-500">Feedback</span>
         </div>
         {/* <Search page={"feedback"} placeholder/> */}
       </div>
-      {feedbacks.map((feedback, index) => {
-        return (
-          <div key={index}>
-            <div className="flex">
-              <h1>Feedback ID : {feedback.id}</h1>
-              <CopyButton textToCopy={feedback.id} />
+        {feedbacks.map((feedback, index) => {
+          return (
+            <div key={index}>
+              <div className="flex">
+                <h1>Feedback ID : {feedback.id}</h1>
+                <CopyButton textToCopy={feedback.id} />
+              </div>
+              <h2>Problem ID : {feedback.problemId}</h2>
+              <h4>User ID : {feedback.userId}</h4>
+              <h3>Description : {feedback.description}</h3>
+              <h3>Status : {feedback.check}</h3>
+              <br />
             </div>
-            <h2>Problem ID : {feedback.problemId}</h2>
-            <h4>User ID : {feedback.userId}</h4>
-            <h3>Description : {feedback.description}</h3>
-            <h3>Status : {feedback.check}</h3>
-            <br />
-          </div>
-        );
-      })}
+          );
+        })}
+      
     </>
   );
 }
