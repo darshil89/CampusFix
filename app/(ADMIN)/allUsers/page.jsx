8d373f21b772fac9d12df4ba40a9fc542b4bff2e
Darshil Mahraur <darshilmahraur67@gmail.com>
2023-12-11 18:04:30 +0530

    error fixed

--- app/(ADMIN)/allUsers/page.jsx
+++ app/(ADMIN)/allUsers/page.jsx
@@ -3,10 +3,13 @@ import Search from "../../../components/SearchCoins";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
-export default function AllUsers() {
-  const session = getServerSession(authOptions);
+export default async function AllUsers() {
+  const session = await getServerSession(authOptions);
   const email = session?.user?.email;
-  if (!session || email !== process.env.ADMIN_EMAIL) redirect("/signin");
+  console.log(session);
+  // if (!session || email !== process.env.ADMIN_EMAIL) redirect("/signin");
+  if(!session) redirect("/signin");
+  if(email !== (process.env.ADMIN_EMAIL)) redirect("/");
   return (
     <>
       <div className="flex justify-between mt-8 ml-4 mb-4 flex-row">
